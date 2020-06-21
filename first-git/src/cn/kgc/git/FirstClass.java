5bef827302743cf43a03a613acb60bc3d89ec59c
wuxin <1014782506@qq.com>
2020-06-21 21:17:26 +0800

    第一次提交了代码

--- first-git/src/cn/kgc/git/FirstClass.java
+++ first-git/src/cn/kgc/git/FirstClass.java
@@ -10,10 +10,12 @@ public class FirstClass {
         System.out.println("hello got project");
         System.out.println("hello got project");
         System.out.println("hello got project");
+        //hello test
     }
 
     public void test1(){
         int i=0;
         i+=10;
+        System.out.println(i);
     }
 }
