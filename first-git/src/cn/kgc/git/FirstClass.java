48b0160ea8e585c06aab87d28a9158b5a63c431e
wuxin <1014782506@qq.com>
2020-06-21 20:59:53 +0800

    java工程

---
+++ first-git/src/cn/kgc/git/FirstClass.java
@@ -0,0 +1,14 @@
+package cn.kgc.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello got project");
+        System.out.println("hello got project");
+        System.out.println("hello got project");
+        System.out.println("hello got project");
+        System.out.println("hello got project");
+        System.out.println("hello got project");
+        System.out.println("hello got project");
+    }
+}
